e5deb9fac51bb1da34833e4b659ecbb8703ec6ae
Alex zhou <63292275+zhouyanupc@users.noreply.github.com>
2020-07-29 09:07:19 +0800

    Add files via upload

---
+++ market_basket_apriori.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+data = pd.read_csv('Market_Basket_Optimisation.csv', header=None)
+
+# print(data.shape) # (7501, 20)
+# transactions
+transactions = []
+for i in range(0,data.shape[0]):
+    temp = []
+    for j in range(0,data.shape[1]):
+        if str(data.values[i, j])!= 'nan':
+            temp.append(str(data.values[i, j]))
+    transactions.append(temp)
+# print(transactions)
+# 挖掘频繁项集和关联规则
+from efficient_apriori import apriori
+itemsets, rules = apriori(transactions, min_support=0.02, min_confidence=0.4)
+print('频繁项集:', itemsets)
+print('关联规则:', rules)
